be7f7eff1b1b065f58ba8e892cf15eae21026a68
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-04-13 00:35:12 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   public/fonts/DMSerifText-Italic.ttf 	new file:   public/fonts/DMSerifText-Regular.ttf 	new file:   public/fonts/Inter-Medium.ttf 	new file:   public/fonts/Inter-Regular.ttf 	new file:   public/fonts/Inter-SemiBold.ttf 	new file:   public/fonts/Inter-Thin.ttf 	new file:   public/fonts/Inter-VariableFont_slnt,wght.ttf 	deleted:    public/models/cloud/model.glb 	new file:   public/models/cloud/model.gltf 	modified:   src/App.jsx 	modified:   src/components/Airplane.jsx 	modified:   src/components/Background.jsx 	modified:   src/components/Cloud.jsx 	modified:   src/components/Experience.jsx 	new file:   src/components/TextSection.jsx 	modified:   src/index.css 	new file:   src/utils/fadeMaterial.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
 import { ScrollControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { EffectComposer, Noise } from "@react-three/postprocessing";
+import { Experience } from "./components/Experience";
 
 function App() {
   return (
     <>
       <Canvas>
         <color attach="background" args={["#ececec"]} />
-        <ScrollControls pages={5} damping={0.3}>
+        <ScrollControls pages={50} damping={0.8}>
           <Experience />
         </ScrollControls>
+        <EffectComposer>
+          <Noise opacity={0.07} />
+        </EffectComposer>
       </Canvas>
     </>
   );
